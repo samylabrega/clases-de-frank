c0d9b19f54e67a00c3eeb081e88147c425da4d5f
samylabrega <161665313+samylabrega@users.noreply.github.com>
2024-04-06 20:06:53 -0400

    Add files via upload

---
+++ EJERCICIOS PAGINA 70/Ejecicio_pagina_70/Ejecicio_pagina_70/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Ejecicio_pagina_70
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Primervalor = new System.Windows.Forms.TextBox();
+            this.Segundovalor = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Suma = new System.Windows.Forms.RadioButton();
+            this.Resultado = new System.Windows.Forms.TextBox();
+            this.Resta = new System.Windows.Forms.RadioButton();
+            this.Multiplicacion = new System.Windows.Forms.RadioButton();
+            this.Division = new System.Windows.Forms.RadioButton();
+            this.Potencia = new System.Windows.Forms.RadioButton();
+            this.Limpiar = new System.Windows.Forms.RadioButton();
+            this.Salir = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Primervalor
+            // 
+            this.Primervalor.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Primervalor.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Primervalor.Location = new System.Drawing.Point(208, 61);
+            this.Primervalor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Primervalor.Name = "Primervalor";
+            this.Primervalor.Size = new System.Drawing.Size(159, 20);
+            this.Primervalor.TabIndex = 0;
+            // 
+            // Segundovalor
+            // 
+            this.Segundovalor.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Segundovalor.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Segundovalor.Location = new System.Drawing.Point(208, 116);
+            this.Segundovalor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Segundovalor.Name = "Segundovalor";
+            this.Segundovalor.Size = new System.Drawing.Size(159, 20);
+            this.Segundovalor.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(363, 576);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Suma
+            // 
+            this.Suma.AutoSize = true;
+            this.Suma.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Suma.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Suma.Location = new System.Drawing.Point(461, 56);
+            this.Suma.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Suma.Name = "Suma";
+            this.Suma.Size = new System.Drawing.Size(52, 17);
+            this.Suma.TabIndex = 3;
+            this.Suma.TabStop = true;
+            this.Suma.Text = "Suma";
+            this.Suma.UseVisualStyleBackColor = false;
+            // 
+            // Resultado
+            // 
+            this.Resultado.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Resultado.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Resultado.Location = new System.Drawing.Point(208, 170);
+            this.Resultado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(159, 20);
+            this.Resultado.TabIndex = 4;
+            // 
+            // Resta
+            // 
+            this.Resta.AutoSize = true;
+            this.Resta.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Resta.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Resta.Location = new System.Drawing.Point(461, 106);
+            this.Resta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Resta.Name = "Resta";
+            this.Resta.Size = new System.Drawing.Size(53, 17);
+            this.Resta.TabIndex = 5;
+            this.Resta.TabStop = true;
+            this.Resta.Text = "Resta";
+            this.Resta.UseVisualStyleBackColor = false;
+            // 
+            // Multiplicacion
+            // 
+            this.Multiplicacion.AutoSize = true;
+            this.Multiplicacion.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Multiplicacion.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Multiplicacion.Location = new System.Drawing.Point(461, 156);
+            this.Multiplicacion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Multiplicacion.Name = "Multiplicacion";
+            this.Multiplicacion.Size = new System.Drawing.Size(89, 17);
+            this.Multiplicacion.TabIndex = 6;
+            this.Multiplicacion.TabStop = true;
+            this.Multiplicacion.Text = "Multiplicacion";
+            this.Multiplicacion.UseVisualStyleBackColor = false;
+            // 
+            // Division
+            // 
+            this.Division.AutoSize = true;
+            this.Division.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Division.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Division.Location = new System.Drawing.Point(461, 202);
+            this.Division.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(62, 17);
+            this.Division.TabIndex = 7;
+            this.Division.TabStop = true;
+            this.Division.Text = "Division";
+            this.Division.UseVisualStyleBackColor = false;
+            // 
+            // Potencia
+            // 
+            this.Potencia.AutoSize = true;
+            this.Potencia.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Potencia.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Potencia.Location = new System.Drawing.Point(461, 250);
+            this.Potencia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Potencia.Name = "Potencia";
+            this.Potencia.Size = new System.Drawing.Size(67, 17);
+            this.Potencia.TabIndex = 8;
+            this.Potencia.TabStop = true;
+            this.Potencia.Text = "Potencia";
+            this.Potencia.UseVisualStyleBackColor = false;
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.AutoSize = true;
+            this.Limpiar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Limpiar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Limpiar.Location = new System.Drawing.Point(461, 298);
+            this.Limpiar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(58, 17);
+            this.Limpiar.TabIndex = 9;
+            this.Limpiar.TabStop = true;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = false;
+            // 
+            // Salir
+            // 
+            this.Salir.AutoSize = true;
+            this.Salir.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Salir.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Salir.Location = new System.Drawing.Point(461, 343);
+            this.Salir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(45, 17);
+            this.Salir.TabIndex = 10;
+            this.Salir.TabStop = true;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "PRIMER VALOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "SEGUNDO VALOR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "RESULTADOS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 428);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.Potencia);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Multiplicacion);
+            this.Controls.Add(this.Resta);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.Suma);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Segundovalor);
+            this.Controls.Add(this.Primervalor);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            //this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Primervalor;
+        private System.Windows.Forms.TextBox Segundovalor;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton Suma;
+        private System.Windows.Forms.TextBox Resultado;
+        private System.Windows.Forms.RadioButton Resta;
+        private System.Windows.Forms.RadioButton Multiplicacion;
+        private System.Windows.Forms.RadioButton Division;
+        private System.Windows.Forms.RadioButton Potencia;
+        private System.Windows.Forms.RadioButton Limpiar;
+        private System.Windows.Forms.RadioButton Salir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EJERCICIOS PAGINA 70/Ejecicio_pagina_70/Ejecicio_pagina_70/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejecicio_pagina_70
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Asocia los manejadores de eventos 
+            Suma.CheckedChanged += Operacion_CheckedChanged;
+            Resta.CheckedChanged += Operacion_CheckedChanged;
+            Multiplicacion.CheckedChanged += Operacion_CheckedChanged;
+            Division.CheckedChanged += Operacion_CheckedChanged;
+            Potencia.CheckedChanged += Operacion_CheckedChanged;
+            Limpiar.CheckedChanged += Operacion_CheckedChanged;
+            Salir.CheckedChanged += Operacion_CheckedChanged;
+        }
+
+        private void Operacion_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+
+            if (radioButton != null && radioButton.Checked)
+            {
+                // Verifica qué RadioButton está seleccionado 
+                switch (radioButton.Name)
+                {
+                    case "Suma":
+                        RealizarOperacion("Suma");
+                        break;
+                    case "Resta":
+                        RealizarOperacion("Resta");
+                        break;
+                    case "Multiplicacion":
+                        RealizarOperacion("Multiplicacion");
+                        break;
+                    case "Division":
+                        RealizarOperacion("Division");
+                        break;
+                    case "Potencia":
+                        RealizarOperacion("Potencia");
+                        break;
+                    case "Limpiar":
+                        LimpiarCampos();
+                        break;
+                    case "Salir":
+                        CerrarFormulario();
+                        break;
+                }
+            }
+        }
+
+        private void RealizarOperacion(string operacion)
+        {
+            double primerValor, segundoValor, resultado = 0;
+
+            if (double.TryParse(Primervalor.Text, out primerValor) && double.TryParse(Segundovalor.Text, out segundoValor))
+            {
+                switch (operacion)
+                {
+                    case "Suma":
+                        resultado = primerValor + segundoValor;
+                        break;
+                    case "Resta":
+                        resultado = primerValor - segundoValor;
+                        break;
+                    case "Multiplicacion":
+                        resultado = primerValor * segundoValor;
+                        break;
+                    case "Division":
+                        if (segundoValor != 0)
+                            resultado = primerValor / segundoValor;
+                        else
+                            MessageBox.Show("No se puede dividir por cero."); 
+                            break;
+                    case "Potencia":
+                        resultado = Math.Pow(primerValor, segundoValor);
+                        break;
+                }
+
+                Resultado.Text = resultado.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese valores numéricos válidos.");
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            Primervalor.Clear();
+            Segundovalor.Clear();
+            Resultado.Clear();
+        }
+
+        private void CerrarFormulario()
+        {
+            this.Close();
+        }
+    }
+}

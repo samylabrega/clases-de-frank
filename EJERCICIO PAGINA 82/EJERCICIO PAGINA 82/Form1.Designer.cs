6d3d7ff399b9578b7b439ffae9fd3ad310a934ca
samylabrega <161665313+samylabrega@users.noreply.github.com>
2024-04-06 22:57:56 -0400

    Add files via upload

---
+++ EJERCICIO PAGINA 82/EJERCICIO PAGINA 82/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace EJERCICIO_PAGINA_82
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Aplicar = new System.Windows.Forms.Button();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.Salida = new System.Windows.Forms.Button();
+            this.Seno = new System.Windows.Forms.RadioButton();
+            this.Coseno = new System.Windows.Forms.RadioButton();
+            this.Tangente = new System.Windows.Forms.RadioButton();
+            this.Arcotangente = new System.Windows.Forms.RadioButton();
+            this.Logaritmo = new System.Windows.Forms.RadioButton();
+            this.Raizcuadrada = new System.Windows.Forms.RadioButton();
+            this.Acoseno = new System.Windows.Forms.RadioButton();
+            this.Aseno = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ESCRIBE EL NUMERO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EL RESULTADO ES ";
+            // 
+            // Aplicar
+            // 
+            this.Aplicar.Location = new System.Drawing.Point(110, 213);
+            this.Aplicar.Name = "Aplicar";
+            this.Aplicar.Size = new System.Drawing.Size(98, 31);
+            this.Aplicar.TabIndex = 2;
+            this.Aplicar.Text = "Aplicar";
+            this.Aplicar.UseVisualStyleBackColor = true;
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Location = new System.Drawing.Point(110, 250);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(98, 31);
+            this.Limpiar.TabIndex = 3;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            // 
+            // Salida
+            // 
+            this.Salida.Location = new System.Drawing.Point(110, 287);
+            this.Salida.Name = "Salida";
+            this.Salida.Size = new System.Drawing.Size(98, 31);
+            this.Salida.TabIndex = 4;
+            this.Salida.Text = "Salida";
+            this.Salida.UseVisualStyleBackColor = true;
+            // 
+            // Seno
+            // 
+            this.Seno.AutoSize = true;
+            this.Seno.Location = new System.Drawing.Point(252, 213);
+            this.Seno.Name = "Seno";
+            this.Seno.Size = new System.Drawing.Size(53, 17);
+            this.Seno.TabIndex = 5;
+            this.Seno.TabStop = true;
+            this.Seno.Text = "Seno ";
+            this.Seno.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Seno.UseVisualStyleBackColor = true;
+            // 
+            // Coseno
+            // 
+            this.Coseno.AutoSize = true;
+            this.Coseno.Location = new System.Drawing.Point(252, 236);
+            this.Coseno.Name = "Coseno";
+            this.Coseno.Size = new System.Drawing.Size(61, 17);
+            this.Coseno.TabIndex = 6;
+            this.Coseno.TabStop = true;
+            this.Coseno.Text = "Coseno";
+            this.Coseno.UseVisualStyleBackColor = true;
+            // 
+            // Tangente
+            // 
+            this.Tangente.AutoSize = true;
+            this.Tangente.Location = new System.Drawing.Point(252, 259);
+            this.Tangente.Name = "Tangente";
+            this.Tangente.Size = new System.Drawing.Size(71, 17);
+            this.Tangente.TabIndex = 7;
+            this.Tangente.TabStop = true;
+            this.Tangente.Text = "Tangente";
+            this.Tangente.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Tangente.UseVisualStyleBackColor = true;
+            // 
+            // Arcotangente
+            // 
+            this.Arcotangente.AutoSize = true;
+            this.Arcotangente.Location = new System.Drawing.Point(252, 282);
+            this.Arcotangente.Name = "Arcotangente";
+            this.Arcotangente.Size = new System.Drawing.Size(96, 17);
+            this.Arcotangente.TabIndex = 8;
+            this.Arcotangente.TabStop = true;
+            this.Arcotangente.Text = "Arco Tangente";
+            this.Arcotangente.UseVisualStyleBackColor = true;
+            // 
+            // Logaritmo
+            // 
+            this.Logaritmo.AutoSize = true;
+            this.Logaritmo.Location = new System.Drawing.Point(252, 305);
+            this.Logaritmo.Name = "Logaritmo";
+            this.Logaritmo.Size = new System.Drawing.Size(71, 17);
+            this.Logaritmo.TabIndex = 9;
+            this.Logaritmo.TabStop = true;
+            this.Logaritmo.Text = "Logaritmo";
+            this.Logaritmo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Logaritmo.UseVisualStyleBackColor = true;
+            // 
+            // Raizcuadrada
+            // 
+            this.Raizcuadrada.AutoSize = true;
+            this.Raizcuadrada.Location = new System.Drawing.Point(343, 213);
+            this.Raizcuadrada.Name = "Raizcuadrada";
+            this.Raizcuadrada.Size = new System.Drawing.Size(95, 17);
+            this.Raizcuadrada.TabIndex = 10;
+            this.Raizcuadrada.TabStop = true;
+            this.Raizcuadrada.Text = "Raiz Cuadrada";
+            this.Raizcuadrada.UseVisualStyleBackColor = true;
+            // 
+            // Acoseno
+            // 
+            this.Acoseno.AutoSize = true;
+            this.Acoseno.Location = new System.Drawing.Point(343, 236);
+            this.Acoseno.Name = "Acoseno";
+            this.Acoseno.Size = new System.Drawing.Size(67, 17);
+            this.Acoseno.TabIndex = 11;
+            this.Acoseno.TabStop = true;
+            this.Acoseno.Text = "Acoseno";
+            this.Acoseno.UseVisualStyleBackColor = true;
+            // 
+            // Aseno
+            // 
+            this.Aseno.AutoSize = true;
+            this.Aseno.Location = new System.Drawing.Point(344, 259);
+            this.Aseno.Name = "Aseno";
+            this.Aseno.Size = new System.Drawing.Size(55, 17);
+            this.Aseno.TabIndex = 12;
+            this.Aseno.TabStop = true;
+            this.Aseno.Text = "Aseno";
+            this.Aseno.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(228, 176);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 177);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FUNCIONES";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(225, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Aseno);
+            this.Controls.Add(this.Acoseno);
+            this.Controls.Add(this.Raizcuadrada);
+            this.Controls.Add(this.Logaritmo);
+            this.Controls.Add(this.Arcotangente);
+            this.Controls.Add(this.Tangente);
+            this.Controls.Add(this.Coseno);
+            this.Controls.Add(this.Seno);
+            this.Controls.Add(this.Salida);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.Aplicar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Aplicar;
+        private System.Windows.Forms.Button Limpiar;
+        private System.Windows.Forms.Button Salida;
+        private System.Windows.Forms.RadioButton Seno;
+        private System.Windows.Forms.RadioButton Coseno;
+        private System.Windows.Forms.RadioButton Tangente;
+        private System.Windows.Forms.RadioButton Arcotangente;
+        private System.Windows.Forms.RadioButton Logaritmo;
+        private System.Windows.Forms.RadioButton Raizcuadrada;
+        private System.Windows.Forms.RadioButton Acoseno;
+        private System.Windows.Forms.RadioButton Aseno;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ EJERCICIO PAGINA 82/EJERCICIO PAGINA 82/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO_PAGINA_82
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Asociar manejadores de eventos a los botones 
+            Aplicar.Click += Aplicar_Click;
+            Limpiar.Click += Limpiar_Click;
+            Salida.Click += Salida_Click;
+        }
+
+        private void Aplicar_Click(object sender, EventArgs e)
+        {
+            CalcularFuncionSeleccionada();
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        private void Salida_Click(object sender, EventArgs e)
+        {
+            CerrarFormulario();
+        }
+
+        private void CalcularFuncionSeleccionada()
+        {
+            double numero, resultado = 0;
+
+            if (double.TryParse(textBox1.Text, out numero))
+            {
+                if (Seno.Checked)
+                    resultado = Math.Sin(numero);
+                else if (Coseno.Checked)
+                    resultado = Math.Cos(numero);
+                else if (Tangente.Checked)
+                    resultado = Math.Tan(numero);
+                else if (Arcotangente.Checked)
+                    resultado = Math.Atan(numero);
+                else if (Logaritmo.Checked)
+                    resultado = Math.Log10(numero);
+                else if (Raizcuadrada.Checked)
+                    resultado = Math.Sqrt(numero);
+                else if (Acoseno.Checked)
+                    resultado = Math.Acos(numero);
+                else if (Aseno.Checked)
+                    resultado = Math.Asin(numero);
+
+                textBox2.Text = resultado.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número válido.");
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            // Desmarcar todos los radio buttons 
+            foreach (Control control in groupBox1.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    ((RadioButton)control).Checked = false;
+                }
+            }
+        }
+
+        private void CerrarFormulario()
+        {
+            this.Close();
+        }
+    }
+}

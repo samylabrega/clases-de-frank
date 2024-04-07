07eb78c8a48e0c2ff7dcdfec5909506a40ac284d
samylabrega <161665313+samylabrega@users.noreply.github.com>
2024-04-06 23:02:05 -0400

    Add files via upload

---
+++ INVENTARIO DE MERCANCIA/INVENTARIO DE MERCANCIA/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace INVENTARIO_DE_MERCANCIA
+{
+    partial class FINVENTARIO
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BCALCULAR = new System.Windows.Forms.Button();
+            this.BNUEVO = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.TBCOMPRAS = new System.Windows.Forms.TextBox();
+            this.TBVENTAS = new System.Windows.Forms.TextBox();
+            this.TBEXISTENCIA = new System.Windows.Forms.TextBox();
+            this.CBARTICULOS = new System.Windows.Forms.ComboBox();
+            this.TBNAJUSTE = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ARTICULOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "COMPRAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "VENTAS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "EXISTENCIA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "FECHA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(136, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "NUMERO DE AJUSTE";
+            // 
+            // BCALCULAR
+            // 
+            this.BCALCULAR.Location = new System.Drawing.Point(36, 308);
+            this.BCALCULAR.Name = "BCALCULAR";
+            this.BCALCULAR.Size = new System.Drawing.Size(75, 23);
+            this.BCALCULAR.TabIndex = 6;
+            this.BCALCULAR.Text = "CALCULAR";
+            this.BCALCULAR.UseVisualStyleBackColor = true;
+            // 
+            // BNUEVO
+            // 
+            this.BNUEVO.Location = new System.Drawing.Point(159, 308);
+            this.BNUEVO.Name = "BNUEVO";
+            this.BNUEVO.Size = new System.Drawing.Size(75, 23);
+            this.BNUEVO.TabIndex = 7;
+            this.BNUEVO.Text = "NUEVO";
+            this.BNUEVO.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(269, 308);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "SALIR";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // TBCOMPRAS
+            // 
+            this.TBCOMPRAS.Location = new System.Drawing.Point(155, 163);
+            this.TBCOMPRAS.Name = "TBCOMPRAS";
+            this.TBCOMPRAS.Size = new System.Drawing.Size(100, 20);
+            this.TBCOMPRAS.TabIndex = 9;
+            // 
+            // TBVENTAS
+            // 
+            this.TBVENTAS.Location = new System.Drawing.Point(155, 190);
+            this.TBVENTAS.Name = "TBVENTAS";
+            this.TBVENTAS.Size = new System.Drawing.Size(100, 20);
+            this.TBVENTAS.TabIndex = 10;
+            // 
+            // TBEXISTENCIA
+            // 
+            this.TBEXISTENCIA.Location = new System.Drawing.Point(155, 224);
+            this.TBEXISTENCIA.Name = "TBEXISTENCIA";
+            this.TBEXISTENCIA.Size = new System.Drawing.Size(100, 20);
+            this.TBEXISTENCIA.TabIndex = 11;
+            // 
+            // CBARTICULOS
+            // 
+            this.CBARTICULOS.FormattingEnabled = true;
+            this.CBARTICULOS.Location = new System.Drawing.Point(155, 140);
+            this.CBARTICULOS.Name = "CBARTICULOS";
+            this.CBARTICULOS.Size = new System.Drawing.Size(121, 21);
+            this.CBARTICULOS.TabIndex = 12;
+            // 
+            // TBNAJUSTE
+            // 
+            this.TBNAJUSTE.Location = new System.Drawing.Point(269, 25);
+            this.TBNAJUSTE.Name = "TBNAJUSTE";
+            this.TBNAJUSTE.Size = new System.Drawing.Size(100, 20);
+            this.TBNAJUSTE.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(269, 68);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // FINVENTARIO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.TBNAJUSTE);
+            this.Controls.Add(this.CBARTICULOS);
+            this.Controls.Add(this.TBEXISTENCIA);
+            this.Controls.Add(this.TBVENTAS);
+            this.Controls.Add(this.TBCOMPRAS);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BNUEVO);
+            this.Controls.Add(this.BCALCULAR);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FINVENTARIO";
+            this.Text = "INVENTARIO DE MERCANCIA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BCALCULAR;
+        private System.Windows.Forms.Button BNUEVO;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox TBCOMPRAS;
+        private System.Windows.Forms.TextBox TBVENTAS;
+        private System.Windows.Forms.TextBox TBEXISTENCIA;
+        private System.Windows.Forms.ComboBox CBARTICULOS;
+        private System.Windows.Forms.TextBox TBNAJUSTE;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ INVENTARIO DE MERCANCIA/INVENTARIO DE MERCANCIA/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INVENTARIO_DE_MERCANCIA
+{
+    public partial class FINVENTARIO : Form
+    {//DECLARAR VARIABLES 
+        int NOAJUSTE = 1;
+        double COMPRAS, VENTAS, EXISTENCIA;
+        public FINVENTARIO()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TBNAJUSTE.Text = Convert.ToString(NOAJUSTE);
+            TBNAJUSTE.Enabled = false;
+            TBEXISTENCIA.Enabled = false;
+            CBARTICULOS.Text = "ESCOJA EL ARTICULO DESEADO";
+            TBCOMPRAS.Text = "0";
+            TBVENTAS.Text = "0";
+        }
+        //CREACION DE METODO ENTRADAS QUE CONIVERTE LOS DATOS DE ENTRADA DESDE EL FORMULARIO
+        //Y ASIGNA EL RESULTADO A LAS VARIABLES CORRESPONDIENTES 
+        private void ENTRADAS()
+        {
+            try
+            {
+                COMPRAS = Convert.ToDouble(TBCOMPRAS.Text);
+                VENTAS = Convert.ToDouble(TBVENTAS.Text);
+            }
+            catch
+            {
+                MessageBox.Show("LA ENTRADA NO ES VALIDA. TRATE DE NUEVO");
+                TBCOMPRAS.Focus();
+
+            }
+
+        }
+        //CREACION DEL METODO CALCULOS, CUYO OBJETIVO ES CALCULAR LA EXISTENCIA Y MOSTRAR
+            //EL RESULTADO EN EL FORMULARIO 
+            private void CALCULOS()
+        {
+            ENTRADAS();
+            EXISTENCIA = (EXISTENCIA + COMPRAS) - VENTAS;
+
+            TBEXISTENCIA.Text = Convert.ToString(EXISTENCIA);
+
+        }
+        private void NUEVO()
+        {
+            NOAJUSTE = NOAJUSTE + 1;
+            TBNAJUSTE.Text = Convert.ToString(NOAJUSTE);
+            CBARTICULOS.Text = "ESCOJA OTRO ARTICULO";
+            TBCOMPRAS.Text = "0";
+            TBVENTAS.Text = "0";
+
+        }
+        private void BCALCULAR_Click(object sender, EventArgs e)
+        {
+            CALCULOS();
+        }
+
+        private void BNUEVO_Click(object sender, EventArgs e)
+        {
+            NUEVO();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}

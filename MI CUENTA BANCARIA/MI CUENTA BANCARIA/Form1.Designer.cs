b244bb55161e6e3341b6e6bf89287628ebfc8ba5
samylabrega <161665313+samylabrega@users.noreply.github.com>
2024-04-08 23:34:49 -0400

    Add files via upload

---
+++ MI CUENTA BANCARIA/MI CUENTA BANCARIA/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace MI_CUENTA_BANCARIA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Procesar = new System.Windows.Forms.Button();
+            this.Nuevo = new System.Windows.Forms.Button();
+            this.Finalizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Depositos = new System.Windows.Forms.TextBox();
+            this.Retiros = new System.Windows.Forms.TextBox();
+            this.Balance = new System.Windows.Forms.TextBox();
+            this.Transaccion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Procesar
+            // 
+            this.Procesar.Location = new System.Drawing.Point(56, 243);
+            this.Procesar.Name = "Procesar";
+            this.Procesar.Size = new System.Drawing.Size(75, 23);
+            this.Procesar.TabIndex = 0;
+            this.Procesar.Text = "Procesar";
+            this.Procesar.UseVisualStyleBackColor = true;
+            this.Procesar.Click += new System.EventHandler(this.Procesar_Click);
+            // 
+            // Nuevo
+            // 
+            this.Nuevo.Location = new System.Drawing.Point(199, 243);
+            this.Nuevo.Name = "Nuevo";
+            this.Nuevo.Size = new System.Drawing.Size(75, 23);
+            this.Nuevo.TabIndex = 1;
+            this.Nuevo.Text = "Nuevo";
+            this.Nuevo.UseVisualStyleBackColor = true;
+            this.Nuevo.Click += new System.EventHandler(this.Nuevo_Click);
+            // 
+            // Finalizar
+            // 
+            this.Finalizar.Location = new System.Drawing.Point(348, 243);
+            this.Finalizar.Name = "Finalizar";
+            this.Finalizar.Size = new System.Drawing.Size(75, 23);
+            this.Finalizar.TabIndex = 2;
+            this.Finalizar.Text = "Finalizar";
+            this.Finalizar.UseVisualStyleBackColor = true;
+            this.Finalizar.Click += new System.EventHandler(this.Finalizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CUENTA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DEPOSITOS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "RETIROS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "BALANCE";
+            // 
+            // Depositos
+            // 
+            this.Depositos.Location = new System.Drawing.Point(140, 96);
+            this.Depositos.Name = "Depositos";
+            this.Depositos.Size = new System.Drawing.Size(100, 20);
+            this.Depositos.TabIndex = 8;
+            // 
+            // Retiros
+            // 
+            this.Retiros.Location = new System.Drawing.Point(140, 122);
+            this.Retiros.Name = "Retiros";
+            this.Retiros.Size = new System.Drawing.Size(100, 20);
+            this.Retiros.TabIndex = 9;
+            // 
+            // Balance
+            // 
+            this.Balance.Location = new System.Drawing.Point(140, 148);
+            this.Balance.Name = "Balance";
+            this.Balance.Size = new System.Drawing.Size(100, 20);
+            this.Balance.TabIndex = 10;
+            // 
+            // Transaccion
+            // 
+            this.Transaccion.Location = new System.Drawing.Point(223, 8);
+            this.Transaccion.Name = "Transaccion";
+            this.Transaccion.Size = new System.Drawing.Size(100, 20);
+            this.Transaccion.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "NO. TRANSACCION";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(156, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "FECHA";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(223, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Transaccion);
+            this.Controls.Add(this.Balance);
+            this.Controls.Add(this.Retiros);
+            this.Controls.Add(this.Depositos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Finalizar);
+            this.Controls.Add(this.Nuevo);
+            this.Controls.Add(this.Procesar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Procesar;
+        private System.Windows.Forms.Button Nuevo;
+        private System.Windows.Forms.Button Finalizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Depositos;
+        private System.Windows.Forms.TextBox Retiros;
+        private System.Windows.Forms.TextBox Balance;
+        private System.Windows.Forms.TextBox Transaccion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ MI CUENTA BANCARIA/MI CUENTA BANCARIA/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MI_CUENTA_BANCARIA
+{
+    public partial class Form1 : Form
+    {
+        private decimal saldo = 0;
+        private int numeroTransaccion = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Configura el ComboBox para que no sea editable 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            // Agrega opciones al ComboBox 
+            comboBox1.Items.AddRange(new object[] { "ADMIN", "Josvir",
+"Katte", "Josmar", "Juan", "Aimee" });
+            // Establecer el TextBox de Balance como de solo lectura 
+            Balance.ReadOnly = true;
+        }
+        private void Finalizar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            // Agrega aquí el código para finalizar la aplicación o realizar otras acciones necesarias
+            Application.Exit();
+        }
+
+        private void Nuevo_Click(object sender, EventArgs e)
+        {
+            Depositos.Text = "";
+            Retiros.Text = "";
+            Balance.Text = "";
+            Transaccion.Text = "";
+            comboBox1.SelectedIndex = -1;
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void Procesar_Click(object sender, EventArgs e)
+        {
+            {
+                if (decimal.TryParse(Depositos.Text, out decimal deposito)
+&&
+                    decimal.TryParse(Retiros.Text, out decimal retiro))
+                {
+                    decimal balanceAnterior = saldo;
+                    saldo = saldo + deposito - retiro;
+                    Balance.Text = saldo.ToString("C");
+                    Transaccion.Text = numeroTransaccion.ToString();
+                    numeroTransaccion++;
+                    MessageBox.Show($"Transacción #{numeroTransaccion}\nFecha: {DateTime.Now}\nBalance anterior: { balanceAnterior}\nDepósito: { deposito}\nRetiro: { retiro}\nNuevo balance: { saldo}"); 
+
+
+                    numeroTransaccion++;
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingrese valores numéricos válidos para depósito y retiro."); 
+                }
+            }
+
+        }
+    }
+}

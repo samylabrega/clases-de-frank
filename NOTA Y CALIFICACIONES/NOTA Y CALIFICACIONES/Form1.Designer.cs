3ba9df8ce5e4ca4dac0c2ce95f8c1e2a8d890a9b
samylabrega <161665313+samylabrega@users.noreply.github.com>
2024-04-09 02:25:00 -0400

    Add files via upload

---
+++ NOTA Y CALIFICACIONES/NOTA Y CALIFICACIONES/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace NOTA_Y_CALIFICACIONES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Procesar = new System.Windows.Forms.Button();
+            this.OtraNota = new System.Windows.Forms.Button();
+            this.Nuevo = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SP = new System.Windows.Forms.TextBox();
+            this.PP = new System.Windows.Forms.TextBox();
+            this.EF = new System.Windows.Forms.TextBox();
+            this.TN = new System.Windows.Forms.TextBox();
+            this.Clasificacion = new System.Windows.Forms.TextBox();
+            this.Practicas = new System.Windows.Forms.TextBox();
+            this.Registro = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Matricula = new System.Windows.Forms.ComboBox();
+            this.Materia = new System.Windows.Forms.ComboBox();
+            this.Profesor = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Procesar
+            // 
+            this.Procesar.Location = new System.Drawing.Point(12, 254);
+            this.Procesar.Name = "Procesar";
+            this.Procesar.Size = new System.Drawing.Size(75, 23);
+            this.Procesar.TabIndex = 0;
+            this.Procesar.Text = "Procesar";
+            this.Procesar.UseVisualStyleBackColor = true;
+            // 
+            // OtraNota
+            // 
+            this.OtraNota.Location = new System.Drawing.Point(97, 254);
+            this.OtraNota.Name = "OtraNota";
+            this.OtraNota.Size = new System.Drawing.Size(75, 23);
+            this.OtraNota.TabIndex = 1;
+            this.OtraNota.Text = "Otra Nota";
+            this.OtraNota.UseVisualStyleBackColor = true;
+            // 
+            // Nuevo
+            // 
+            this.Nuevo.Location = new System.Drawing.Point(178, 254);
+            this.Nuevo.Name = "Nuevo";
+            this.Nuevo.Size = new System.Drawing.Size(75, 23);
+            this.Nuevo.TabIndex = 2;
+            this.Nuevo.Text = "Nuevo";
+            this.Nuevo.UseVisualStyleBackColor = true;
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(259, 254);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(75, 23);
+            this.Salir.TabIndex = 3;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "MATRICULA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MATERIA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PROFESOR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "PRIMER PARCIAL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "SEGUNDO PARCIAL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "PRACTICAS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "EXAMEN FINAL";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 191);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "TOTAL NOTA";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(35, 217);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "CLASIFICACION";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(330, 6);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "NO. REGISTRO";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(330, 30);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "FECHA";
+            // 
+            // SP
+            // 
+            this.SP.Location = new System.Drawing.Point(150, 113);
+            this.SP.Name = "SP";
+            this.SP.Size = new System.Drawing.Size(100, 20);
+            this.SP.TabIndex = 18;
+            // 
+            // PP
+            // 
+            this.PP.Location = new System.Drawing.Point(150, 87);
+            this.PP.Name = "PP";
+            this.PP.Size = new System.Drawing.Size(100, 20);
+            this.PP.TabIndex = 19;
+            // 
+            // EF
+            // 
+            this.EF.Location = new System.Drawing.Point(150, 165);
+            this.EF.Name = "EF";
+            this.EF.Size = new System.Drawing.Size(100, 20);
+            this.EF.TabIndex = 20;
+            // 
+            // TN
+            // 
+            this.TN.Location = new System.Drawing.Point(150, 191);
+            this.TN.Name = "TN";
+            this.TN.Size = new System.Drawing.Size(100, 20);
+            this.TN.TabIndex = 21;
+            // 
+            // Clasificacion
+            // 
+            this.Clasificacion.Location = new System.Drawing.Point(150, 217);
+            this.Clasificacion.Name = "Clasificacion";
+            this.Clasificacion.Size = new System.Drawing.Size(100, 20);
+            this.Clasificacion.TabIndex = 22;
+            // 
+            // Practicas
+            // 
+            this.Practicas.Location = new System.Drawing.Point(150, 139);
+            this.Practicas.Name = "Practicas";
+            this.Practicas.Size = new System.Drawing.Size(100, 20);
+            this.Practicas.TabIndex = 23;
+            // 
+            // Registro
+            // 
+            this.Registro.Location = new System.Drawing.Point(421, 2);
+            this.Registro.Name = "Registro";
+            this.Registro.Size = new System.Drawing.Size(100, 20);
+            this.Registro.TabIndex = 24;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(377, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 25;
+            // 
+            // Matricula
+            // 
+            this.Matricula.FormattingEnabled = true;
+            this.Matricula.Location = new System.Drawing.Point(150, 6);
+            this.Matricula.Name = "Matricula";
+            this.Matricula.Size = new System.Drawing.Size(121, 21);
+            this.Matricula.TabIndex = 26;
+            // 
+            // Materia
+            // 
+            this.Materia.FormattingEnabled = true;
+            this.Materia.Location = new System.Drawing.Point(150, 36);
+            this.Materia.Name = "Materia";
+            this.Materia.Size = new System.Drawing.Size(121, 21);
+            this.Materia.TabIndex = 27;
+            // 
+            // Profesor
+            // 
+            this.Profesor.FormattingEnabled = true;
+            this.Profesor.Location = new System.Drawing.Point(150, 63);
+            this.Profesor.Name = "Profesor";
+            this.Profesor.Size = new System.Drawing.Size(121, 21);
+            this.Profesor.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Profesor);
+            this.Controls.Add(this.Materia);
+            this.Controls.Add(this.Matricula);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Registro);
+            this.Controls.Add(this.Practicas);
+            this.Controls.Add(this.Clasificacion);
+            this.Controls.Add(this.TN);
+            this.Controls.Add(this.EF);
+            this.Controls.Add(this.PP);
+            this.Controls.Add(this.SP);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Nuevo);
+            this.Controls.Add(this.OtraNota);
+            this.Controls.Add(this.Procesar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Procesar;
+        private System.Windows.Forms.Button OtraNota;
+        private System.Windows.Forms.Button Nuevo;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        //private System.Windows.Forms.ComboBox matriculas;
+        //private System.Windows.Forms.ComboBox materias;
+        //private System.Windows.Forms.ComboBox profesores;
+        private System.Windows.Forms.TextBox SP;
+        private System.Windows.Forms.TextBox PP;
+        private System.Windows.Forms.TextBox EF;
+        private System.Windows.Forms.TextBox TN;
+        private System.Windows.Forms.TextBox Clasificacion;
+        private System.Windows.Forms.TextBox Practicas;
+        private System.Windows.Forms.TextBox Registro;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox Matricula;
+        private System.Windows.Forms.ComboBox Materia;
+        private System.Windows.Forms.ComboBox Profesor;
+    }
+}
+
---
+++ NOTA Y CALIFICACIONES/NOTA Y CALIFICACIONES/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NOTA_Y_CALIFICACIONES
+{
+    public partial class Form1 : Form
+    {
+
+
+        private Random random = new Random();
+        private List<string> matriculas = new List<string> {
+"Matricula1", "Matricula2", "Matricula3" };
+        private List<string> materias = new List<string> { "Materia1",
+"Materia2", "Materia3" };
+        private List<string> profesores = new List<string> {
+"Profesor1", "Profesor2", "Profesor3" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Asociar manejadores de eventos a los botones 
+            Procesar.Click += Procesar_Click;
+            OtraNota.Click += OtraNota_Click;
+            Nuevo.Click += Nuevo_Click;
+            Salir.Click += Salir_Click;
+
+            // Configurar propiedades de los campos 
+            Clasificacion.ReadOnly = true;
+            Registro.ReadOnly = true;
+            Registro.Text = GenerarNumeroAleatorio().ToString();
+
+            // Llenar ComboBox con datos predefinidos 
+            Matricula.DataSource = matriculas;
+            Materia.DataSource = materias;
+            Profesor.DataSource = profesores;
+        }
+
+        private void Procesar_Click(object sender, EventArgs e)
+        {
+            CalcularClasificacion();
+        }
+
+        private void OtraNota_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            Registro.Text = GenerarNumeroAleatorio().ToString();
+        }
+
+        private void Nuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            Registro.Text = GenerarNumeroAleatorio().ToString();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            CerrarFormulario();
+        }
+
+        private void CalcularClasificacion()
+        {
+            try
+            {
+                // Obtener los valores de los campos 
+                double primerParcial = Convert.ToDouble(PP.Text);
+                double segundoParcial = Convert.ToDouble(SP.Text);
+                double practicas = Convert.ToDouble(Practicas.Text);
+                double examenFinal = Convert.ToDouble(EF.Text);
+
+                // Calcular total y clasificación 
+                double totalNota = primerParcial + segundoParcial +
+practicas + examenFinal;
+
+                string clasificacion;
+
+                if (totalNota >= 70)
+                    clasificacion = "Aprobado";
+                else
+                    clasificacion = "Reprobado";
+
+                // Mostrar resultados 
+                TN.Text = totalNota.ToString();
+                Clasificacion.Text = clasificacion;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al calcular la clasificación: " +
+ex.Message);
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            // Limpiar todos los campos 
+            PP.Clear();
+            SP.Clear();
+            Practicas.Clear();
+            EF.Clear();
+            TN.Clear();
+            Clasificacion.Clear();
+        }
+
+        private void CerrarFormulario()
+        {
+            this.Close();
+        }
+
+        private int GenerarNumeroAleatorio()
+        {
+            return random.Next(1000, 9999);
+        }
+    }
+}
